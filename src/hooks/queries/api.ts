376f9d997734f9a72ac3745065df9eff2ba6d5a3
Muhideen Mujeeb Adeoye <mujeeb.muhideen@gmail.com>
2025-08-21 16:14:04 +0100

    debuging sb auth token

--- src/hooks/queries/api.ts
+++ src/hooks/queries/api.ts
@@ -20,6 +20,8 @@ export async function api<T = any>(
     supabaseLocalAuthTokenKey,
   );
 
+  console.log("supabaseLocalAuthTokenRaw", supabaseLocalAuthTokenRaw);
+
   let supabaseLocalAuthToken: SupabaseLocalAuthToken | null = null;
 
   if (supabaseLocalAuthTokenRaw) {
@@ -27,6 +29,8 @@ export async function api<T = any>(
       supabaseLocalAuthTokenRaw,
     ) as SupabaseLocalAuthToken;
   }
+
+  console.log("supabaseLocalAuthToken", supabaseLocalAuthToken);
 
   const defaultHeaders: Record<string, string> = {
     Accept: "*/*",
