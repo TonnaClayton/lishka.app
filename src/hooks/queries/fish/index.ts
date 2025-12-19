1f4b73b933520bcd270f29b2d217e6ea211d76de
sacha.cassar1@me.com <sacha.cassar1@me.com>
2025-12-19 18:10:04 +0100

    chore: remove non existing import

--- src/hooks/queries/fish/index.ts
+++ src/hooks/queries/fish/index.ts
@@ -1,5 +1,4 @@
 export * from "./use-fish-data";
 export * from "./use-fish-detail";
-export * from "./use-fao-fish-data";
 export * from "./use-fao-fish-stream";
 export * from "./use-fao-toxic-fish-stream";
