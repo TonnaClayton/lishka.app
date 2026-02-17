1dfcc6c6030f0c8065c41681e8e0e21eefb51a00
sacha.cassar1@me.com <sacha.cassar1@me.com>
2026-02-17 22:43:27 +0100

    feat: remove install app modal

--- src/App.tsx
+++ src/App.tsx
@@ -32,7 +32,6 @@ import AuthWrapper from "./pages/auth/auth-wrapper";
 import { PostHogProvider, PostHogErrorBoundary } from "posthog-js/react";
 import { initPosthog, posthog } from "./lib/posthog";
 import { Toaster } from "./components/ui/toaster";
-import AddToHomeScreenPrompt from "./components/add-to-homescreen";
 
 // Lazy load heavy components for better initial loading performance
 const HomePage = lazy(() => import("./pages/home"));
@@ -592,7 +591,6 @@ function App() {
           </Suspense>
           <ReactQueryDevtools initialIsOpen={false} />
           <Toaster />
-          <AddToHomeScreenPrompt />
         </QueryClientProvider>
         {/* </ErrorBoundary> */}
       </PostHogErrorBoundary>
